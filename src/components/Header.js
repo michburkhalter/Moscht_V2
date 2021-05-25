45e1eb954ec57ab8b2e9b2422c75dffe02f67600
michburkhalter <michburkhalter@gmail.com>
2021-05-25 21:29:11 +0200

    1 file modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,25 +6,61 @@ function Header() {
   return (
     <header>
       <nav className="navbar navbar-expand-sm fixed-top navbar-light bg-light">
-        <Link className="navbar-brand" to="/">Moscht</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <Link className="navbar-brand" to="/">
+          Moscht
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-          {auth().currentUser
-            ? <div className="navbar-nav">
-              <Link className="nav-item nav-link mr-3" to="/overview">Overview</Link>
-              <Link className="nav-item nav-link mr-3" to="/addfill">Add Fill</Link>
-              <Link className="nav-item nav-link mr-3" to="/detailstable">Fills Table</Link>
-              <Link className="nav-item nav-link mr-3" to="/managecars">Manage Cars</Link>
-              <Link className="nav-item nav-link mr-3" to="/logs">Log Event</Link>
-              <Link className="nav-item nav-link mr-3" to="/profile">Profile</Link>
-              <button className="btn btn-primary mr-3" onClick={() => auth().signOut()}>Logout</button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNavAltMarkup"
+        >
+          {auth().currentUser ? (
+            <div className="navbar-nav">
+              <Link className="nav-item nav-link mr-3" to="/overview">
+                Overview
+              </Link>
+              <Link className="nav-item nav-link mr-3" to="/addfill">
+                Add Fill
+              </Link>
+              <Link className="nav-item nav-link mr-3" to="/detailstable">
+                Fills Table
+              </Link>
+              <Link className="nav-item nav-link mr-3" to="/logs">
+                Log Event
+              </Link>
+              <Link className="nav-item nav-link mr-3" to="/managecars">
+                Manage Cars
+              </Link>
+              <Link className="nav-item nav-link mr-3" to="/profile">
+                Profile
+              </Link>
+              <button
+                className="btn btn-primary mr-3"
+                onClick={() => auth().signOut()}
+              >
+                Logout
+              </button>
             </div>
-            : <div className="navbar-nav">
-              <Link className="nav-item nav-link mr-3" to="/login">Sign In</Link>
-              <Link className="nav-item nav-link mr-3" to="/signup">Sign Up</Link>
-            </div>}
+          ) : (
+            <div className="navbar-nav">
+              <Link className="nav-item nav-link mr-3" to="/login">
+                Sign In
+              </Link>
+              <Link className="nav-item nav-link mr-3" to="/signup">
+                Sign Up
+              </Link>
+            </div>
+          )}
         </div>
       </nav>
     </header>
