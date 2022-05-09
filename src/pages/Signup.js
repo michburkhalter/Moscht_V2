d8b589842c9a5bbc6a36afcb08fabd0d7eb295bd
Michael Burkhalter <michburkhalter@gmail.com>
2022-05-09 13:41:55 +0200

    check simple text value from db as access code

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import {signInWithGitHub, signInWithGoogle, signup} from "../helpers/auth";
-import {ref, set} from "firebase/database";
+import {ref, set,onValue,update} from "firebase/database";
 import {db} from "../services/firebase";
 
 export default class SignUp extends Component {
@@ -13,6 +13,8 @@ export default class SignUp extends Component {
             email: '',
             password: '',
             name: '',
+            master_hash: '',
+            zugangscode: '',
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -26,17 +28,37 @@ export default class SignUp extends Component {
         });
     }
 
+    async componentDidMount() {
+        const public_application = ref(db, 'public_application/');
+        onValue(public_application, snapshot => {
+            console.log("onValue db.public_application");
+
+            snapshot.forEach(snap => {
+                if (snap.key == 'master_hash'){
+                    this.setState({
+                        "master_hash": snap.val()
+                    });
+                }
+            });
+        });
+    }
+
     async handleSubmit(event) {
         event.preventDefault();
         this.setState({error: ''});
-        try {
-            await signup(this.state.email, this.state.password).then(value => {
-                set(ref(db, 'user_settings/' + value.user.uid), {
-                    UserName: this.state.name
+
+        if (this.state.zugangscode != this.state.master_hash){
+            this.setState({error: "Wrong access code"});
+        }else{
+            try {
+                await signup(this.state.email, this.state.password).then(value => {
+                    set(ref(db, 'user_settings/' + value.user.uid), {
+                        UserName: this.state.name
+                    });
                 });
-            });
-        } catch (error) {
-            this.setState({error: error.message});
+            } catch (error) {
+                this.setState({error: error.message});
+            }
         }
     }
 
@@ -77,6 +99,10 @@ export default class SignUp extends Component {
                     <div className="form-group my-2">
                         <input className="form-control" placeholder="Password" name="password"
                                onChange={this.handleChange} value={this.state.password} type="password"></input>
+                    </div>
+                    <div className="form-group my-2">
+                        <input className="form-control" placeholder="Access Code" name="zugangscode"
+                               onChange={this.handleChange} value={this.state.zugangscode} type="text"></input>
                     </div>
                     <div className="form-group">
                         {this.state.error ? <p className="text-danger">{this.state.error}</p> : null}
